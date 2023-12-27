00eb75ff81065b37c3cc71524b499b5799749a81
parth <parthitaliya93@gmail.com>
2023-12-27 10:50:37 +0530

    updated

--- PR6_1.c
+++ PR6_1.c
@@ -1,36 +1,31 @@
-/*Q.1 Write a Program to check whether a string is a palindrome or not without using string functions.
-Output1:
-Enter a string : nayan
-The string is palindrome
-
-Oputput2:
-Enter a string : harris
-The string is not palindrome
-
+/*
+Q.1 Write a Program to check whether a string is a palindrome or not without using string functions.
+output1:
+Enter any string: nayan
+The string is pelindrome
+output2:
+Enter any string: kartik
+The string is not pelindrome
 */
 
-#include <stdio.h>
-#include <string.h>
- 
-int main()
-{
-    char s[700];  
-    
-	int n,i,c=0;
- 
-    printf("Enter a string : ");
-    gets(s);
-   
-    n=strlen(s);
-    for(i=0; i<n/2; i++)  
-    {	if(s[i]==s[n-i-1])
-  c++;
- 	}
- 	if(c==i){
-	   printf("The string is palindrome");
-    }else{
-	 printf("The string is not palindrome");
-}
-}
+#include<stdio.h>
 
-
+void main(){
+	int i;
+	char name[100];
+	int length=0,sum=1;
+	
+	printf("Enter any string: ");
+	gets(name);
+	          
+	for(i=0; name[i]!='\0'; i++){
+		length++;
+	}
+	for(i=0; i<length/2; i++){
+		if(name[i]!= name[length-1-i]){
+		sum=0;
+	}
+}
+	(sum==1)?printf("The string is pelindrome")
+			:printf("The string is not pelindrome");
+}
